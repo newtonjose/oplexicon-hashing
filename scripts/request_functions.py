2c4fea253a51d81bbd897bf6c112339057e2c69d
newtonjose <josephnewton001@gmail.com>
2022-09-16 20:47:08 -0300

    chore: update script

--- scripts/request_functions.py
+++ scripts/request_functions.py
@@ -4,12 +4,20 @@ NUM_REQUESTS = 100
 RESULTS = {
     "times": [],
     "collisions": [],
+    "hash keys": [],
 }
 
 if __name__ == '__main__':
     for e in ['pjw', 'md4', 'md5', 'sha1', 'sha2']:
-        # r = requests.get('http://localhost:8000/pjw?key=%23brandsclub')
-        r = requests.get(f'http://localhost:8000/{e}')
+        r = requests.get(f'http://localhost:8000/spread_collisions?algorith={e}')
+        for w in ['zebrar', 'despersuadir']:
+            ms_data = []
+            for i in range(100):
+                r = requests.get(f'http://localhost:8000/{e}?key={w}')
 
-        data = r.json()
-        print(e, data['total_collisions'], data['performance_microseconds'])
+                data = r.json()
+                ms_data.append(data['key_infos']['ms'])
+
+            with open(f'./{e}_{w}_lexicon.csv', 'w') as f:
+                for val in ms_data:
+                    f.write("%s;%s\n" % (w, val))
